a31c8973ad552ccb747894ca366ff63adc6c6605
Joe Fabiano <jfabiano@gmail.com>
2016-08-18 11:03:45 -0400

    base version

---
+++ src/tiyinc/noobs/Day9Runner.java
@@ -0,0 +1,12 @@
+package tiyinc.noobs;
+
+/**
+ * Created by jfabiano on 8/18/2016.
+ */
+public class Day9Runner
+{
+    public static void main(String[] args)
+    {
+        System.out.println("running");
+    }
+}
