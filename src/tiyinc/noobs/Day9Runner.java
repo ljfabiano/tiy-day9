634bf99bab1730c67e99202e1f221e1f51a85e80
Joe Fabiano <jfabiano@gmail.com>
2016-08-18 11:07:06 -0400

    Commit 2

--- src/tiyinc/noobs/Day9Runner.java
+++ src/tiyinc/noobs/Day9Runner.java
@@ -8,5 +8,11 @@ public class Day9Runner
     public static void main(String[] args)
     {
         System.out.println("running");
+        Day9Runner myRunner = new Day9Runner();
+        myRunner.testWriteFile();
+    }
+    public void testWriteFile()
+    {
+        System.out.println("testWriteFile()");
     }
 }
